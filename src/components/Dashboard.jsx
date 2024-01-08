d1a3deff420191aaa1c230de415d07ecdfee0855
akshat-g-07 <akshatg805@gmail.com>
2024-01-09 00:21:59 +0530

    Integrated Dashboard Header

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,71 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        {/**
+         * Header Starts
+         */}
+        <div className="flex items-center w-[1216px] h-[64px] border-b-[1px] px-[32px] border-[#D9D9D9] py-[12px] bg-white">
+          <div className="w-[360px] h-[22px] flex items-center">
+            <p className="text-[#1A181E]">Payments</p>
+            <div className="w-[16px] h-full" />
+            <div className="w-[14px] h-[14px]">
+              <img
+                src="/images/dashboard-outlined-help.png"
+                alt="dashboard-outlined-help"
+              />
+            </div>
+            <div className="w-[6px] h-full" />
+            <p className="text-[#4D4D4D] w-[74px] h-[16px] font-normal text-xs">
+              How it works
+            </p>
+          </div>
+          <div className="w-[16px] h-full" />
+          <div className="bg-[#F2F2F2] w-[400px] h-[40px] rounded-[6px] px-[16px] py-[9px] flex items-center">
+            <div className="w-[16px] h-[16px]">
+              <img
+                src="/images/dashboard-icons-search.png"
+                alt="dashboard-icons-search"
+              />
+            </div>
+            <div className="w-[6px] h-full" />
+            <p className="text-[#808080] w-[213px] h-[22px] font-normal text-sm">
+              Search features, tutorials, etc.
+            </p>
+          </div>
+          <div className="w-[16px] h-full" />
+          <div className="w-[360px] h-[40px] flex flex-row-reverse items-center">
+            <div
+              className="w-[40px] h-[40px] flex items-center justify-center"
+              style={{
+                backgroundImage: "url('images/dashboard-icons-ellipse-2.png')",
+              }}
+            >
+              <img
+                src="/images/dashboard-icons-menu.png"
+                alt="dashboard-icons-menu"
+              />
+            </div>
+            <div className="w-[12px] h-full" />
+            <div
+              className="w-[40px] h-[40px] flex items-center justify-center"
+              style={{
+                backgroundImage: "url('images/dashboard-icons-ellipse-2.png')",
+              }}
+            >
+              <img
+                src="/images/dashboard-icons-group.png"
+                alt="dashboard-icons-group"
+              />
+            </div>
+          </div>
+        </div>
+        {/**
+         * Header Ends
+         */}
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
 import LeftPanel from "./LeftPanel";
+import Dashboard from "./Dashboard";
 
 const Solution = ({ pixelPerfect }) => {
   return (
     <>
-      <div className="p-24 w-fit h-fit">
-        <div className="w-[1440px] h-[1482px]">
+      <div className="p-24 w-fit h-fit bg-slate-400">
+        <div className="w-[1440px] h-[1482px] flex">
           <LeftPanel />
+          <Dashboard />
         </div>
       </div>
     </>
