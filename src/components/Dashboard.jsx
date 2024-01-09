734c7cb9135ac3042cbdd968ba18f34b2e68890b
akshat-g-07 <akshatg805@gmail.com>
2024-01-09 18:54:59 +0530

    Content Boxes of Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
     <>
-      <div>
+      <div className="bg-[#FAFAFA]">
         {/**
          * Header Starts
          */}
@@ -62,6 +62,66 @@ const Dashboard = () => {
         </div>
         {/**
          * Header Ends
+         */}
+
+        {/**
+         * Content Box 1 Starts
+         */}
+        <div className="flex items-center w-[1152px] h-[36px] mx-auto mt-[32px] justify-between">
+          <div className="font-medium text-xl w-[91px] h-[28px] text-[#1A181E]">
+            Overview
+          </div>
+          <div className="border-[1px] border-[#D9D9D9] h-[36px] rounded-[4px] w-[137px] flex bg-[#FFFFFF]">
+            <div className="text-[#4D4D4D] font-normal w-[86px] h-[24px] ml-[14px] my-[6px] text-base">
+              Last Month
+            </div>
+            <div className="w-[16px] h-[16px] my-[10px] ml-[7px]">
+              <img src="images/icons-arrow.png" alt="icons-arrow" />
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[24px]" />
+        {/**
+         * Content Box 1 Ends
+         */}
+
+        {/**
+         * Content Box 2 Starts
+         */}
+        <div className="flex w-[fit] h-[118px] mx-auto justify-center">
+          <div
+            className="bg-[#FFF] w-[566px] h-[118px] p-[20px] rounded-[8px]"
+            style={{
+              boxShadow: "0px 2px 6px 0px rgba(26, 24, 30, 0.04)",
+            }}
+          >
+            <p className="font-normal text-base text-[#4D4D4D] h-[24px]">
+              Online orders
+            </p>
+            <p className="w-full h-[16px]" />
+            <p className="font-medium text-[32px] leading-[38px] text-[#1A181E] h-[38px]">
+              231
+            </p>
+          </div>
+          <div className="w-[20px] h-full" />
+          <div
+            className="bg-[#FFF] w-[566px] h-[118px] p-[20px] rounded-[8px]"
+            style={{
+              boxShadow: "0px 2px 6px 0px rgba(26, 24, 30, 0.04)",
+            }}
+          >
+            <p className="font-normal text-base text-[#4D4D4D] h-[24px]">
+              Amount received
+            </p>
+            <p className="w-full h-[16px]" />
+            <p className="font-medium text-[32px] leading-[38px] text-[#1A181E] h-[38px]">
+              ₹23,92,312.19
+            </p>
+          </div>
+        </div>
+        <div className="w-full h-[32px]" />
+        {/**
+         * Content Box 2 Ends
          */}
       </div>
     </>
